f7955835d3d7f4995836c4a1391cb6b830cc9d87
SergeyKovalenko <kovalenko.sv1@novaposhta.ua>
2018-11-06 17:41:01 +0200

    At last preloading for images. Some play with animation on front

--- index.php
+++ index.php
@@ -10,10 +10,33 @@
 <body>
     <canvas id="canvas_view" width="800" height="800" style="border: 1px solid"></canvas>
     <canvas id="canvas" width="800" height="800" style="border: 1px solid;width: 200px;height: 200px"></canvas>
+    <div style="display: none!important;">
+        <?php // preloading images
+        //// this part is needed for preloading images instead of onload event which slows down image rendering and
+        //                // makes images to blink all the time. ?>
+        <img src="src/img/hallview_end_left.png">
+        <img src="src/img/hallview_end_right.png">
+        <img src="src/img/hallview_front_wall_left.png">
+        <img src="src/img/hallview_front_wall_right.png">
+        <img src="src/img/hallview_full_left.png">
+        <img src="src/img/hallview_full_right.png">
+        <img src="src/img/hallview_half_left.png">
+        <img src="src/img/hallview_half_right.png">
+        <img src="src/img/mob3dFull.png">
+        <img src="src/img/mob3dMedium.png">
+        <img src="src/img/mob3dSmall.png">
+        <img src="src/img/mob_jaws.jpg">
+        <img src="src/img/mob_jaws_half.jpg">
+        <img src="src/img/mob_jaws_last.jpg">
+        <img src="src/img/player.jpg">
+        <img src="src/img/wall.jpg">
+    </div>
     <script>
         var wall = new Image;
         var player = new Image;
         var mobJaws = new Image;
+
+        previousViewGameImage = '';
 
 
         wall.src = 'src/img/wall.jpg';
@@ -55,12 +78,6 @@
         var canvas3D = document.getElementById('canvas_view');
         var canvasContext3D = canvas3D.getContext('2d');
         canvasContext3D.save();
-        // setInterval(function () {
-        //     if (buffer != '') {
-        //         socket.send(buffer);
-        //     }
-        //     buffer = '';
-        // }, 200);
 
         window.addEventListener('keydown', function (e) {
             if (CODE_ARROWS.indexOf(e.keyCode) === -1) { // not arrow
@@ -79,60 +96,82 @@
             walls.forEach(function(item) {
                 canvasContext.drawImage(wall, item.x, item.y);
             });
-            if (typeof mapData.mobs !== typeof undefined) {
-                // this part is needed for preloading images instead of onload event which slows down image rendering and
-                // makes images to blink all the time.
-                var mobJaws1 = new Image; // preloading
-                var mobJaws2 = new Image; // preloading
-                var mobJaws3 = new Image; // preloading
-                mobJaws1.src = 'src/img/mob_jaws.jpg'; // preloading
-                mobJaws2.src = 'src/img/mob_jaws_half.jpg'; // preloading
-                mobJaws3.src = 'src/img/mob_jaws_last.jpg'; // preloading
-                var mobs = mapData.mobs;
 
+            var viewGameImage = new Image();
+            // before loading
+            // //if (mapData need animation do animation) { // todo add logic on server to rule animation
+            // console.log('previousViewGameImage'); // TODO remove debug!!
+            // console.log(previousViewGameImage); // TODO remove debug!!
+            // canvasContext3D.save();
+            // if (previousViewGameImage !== '') {
+            //     console.log("!=="); // TODO remove debug!!
+            //     var scale = 1;
+            //
+            //     var timerId = setInterval(animate, 50);
+            //     function animate() {
+            //         console.log("animate"+scale); // TODO remove debug!!
+            //         if (scale === 1.05) {
+            //             clearInterval(timerId);
+            //         }
+            //         canvasContext3D.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+            //         canvasContext3D.translate(CANVAS_SIZE/2, CANVAS_SIZE/2);
+            //         canvasContext3D.scale(scale, scale);
+            //         canvasContext3D.translate(-CANVAS_SIZE/2, -CANVAS_SIZE/2);
+            //
+            //         // before loading next image, animate current
+            //         console.log('animate.previousViewGameImage.src');
+            //         console.log(previousViewGameImage.src);
+            //         canvasContext3D.drawImage(previousViewGameImage, 0, 0);
+            //         scale = scale + 0.01;
+            //     }
+            //
+            //     setTimeout(drawView,5000);
+            //     function drawView() {
+            //         clearInterval(timerId);
+            //         canvasContext3D.restore();
+            //         canvasContext3D.clearRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+            //         canvasContext3D.scale(1, 1);
+            //         viewGameImage.src = mapData.gameView.src;
+            //         console.log('drawView');
+            //         console.log(viewGameImage.src);
+            //         canvasContext3D.scale(1, 1);
+            //         canvasContext3D.drawImage(viewGameImage, mapData.gameView.x, mapData.gameView.y);
+            //         canvasContext3D.scale(1, 1);
+            //         previousViewGameImage = viewGameImage;
+            //     }
+            // } else {
+            //     canvasContext3D.restore();
+            //     console.log("eslse"); // TODO remove debug!!
+            //     viewGameImage.src = mapData.gameView.src;
+            //     console.log('eslsedrawView');
+            //     console.log(viewGameImage.src);
+            //     canvasContext3D.drawImage(viewGameImage, mapData.gameView.x, mapData.gameView.y);
+            //     previousViewGameImage = viewGameImage;
+            // }
+            //}
+            viewGameImage.src = mapData.gameView.src;
+            console.log('eslsedrawView');
+            console.log(viewGameImage.src);
+            canvasContext3D.drawImage(viewGameImage, mapData.gameView.x, mapData.gameView.y);
+
+            if (typeof mapData.mobs !== typeof undefined) {
+                var mobs = mapData.mobs;
                 mobs.forEach(function(itemMobs) {
                     mobJaws.src = itemMobs.src;
                     canvasContext.drawImage(mobJaws, itemMobs.x, itemMobs.y);
-                    mobJaws.onload = function() {
-                        canvasContext.drawImage(mobJaws, itemMobs.x, itemMobs.y);
-                    };
                 });
 
                 if (typeof mapData.mobsView !== typeof undefined) {
-                    var viewG = new Image;
-                    viewG.src = mapData.gameView.src;
-                    canvasContext3D.drawImage(viewG, mapData.gameView.x, mapData.gameView.y);
-                    viewG.onload = function() {
-                        canvasContext3D.drawImage(viewG, mapData.gameView.x, mapData.gameView.y);
-                        var mobsView = mapData.mobsView;
-                        var viewMobsImg = new Image;
-                        mobsView.forEach(function(itemMobsView) {
-                            viewMobsImg.src = itemMobsView.src;
-                            canvasContext3D.drawImage(viewMobsImg, itemMobsView.x, itemMobsView.y);
-                            viewMobsImg.onload = function() {
-                                canvasContext3D.drawImage(viewMobsImg, itemMobsView.x, itemMobsView.y);
-                            };
-                        });
-                    };
-                } else {
-                    var viewG = new Image;
-                    viewG.onload = function() {
-                        canvasContext3D.drawImage(viewG, mapData.gameView.x, mapData.gameView.y);
-                    };
-                    viewG.src = mapData.gameView.src;
-                    canvasContext3D.drawImage(viewG, mapData.gameView.x, mapData.gameView.y);
+                    var mobsView = mapData.mobsView;
+                    var viewMobsImg = new Image;
+                    mobsView.forEach(function(itemMobsView) {
+                        viewMobsImg.src = itemMobsView.src;
+                        canvasContext3D.drawImage(viewMobsImg, itemMobsView.x, itemMobsView.y);
+                    });
                 }
-            } else {
-                var viewG = new Image;
-                viewG.onload = function() {
-                    canvasContext3D.drawImage(viewG, mapData.gameView.x, mapData.gameView.y);
-                };
-                viewG.src = mapData.gameView.src;
-                canvasContext3D.drawImage(viewG, mapData.gameView.x, mapData.gameView.y);
             }
+
             canvasContext.drawImage(player, mapData.player.x, mapData.player.y);
-
-
         };
     </script>
 </body>
